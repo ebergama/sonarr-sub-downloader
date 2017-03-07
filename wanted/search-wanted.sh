da467d17704223a8d4f58108a9b397feff156400
Ezequiel Bergamaschi <ezequiel@medallia.com>
2017-03-06 23:55:08 -0300

    Update search-wanted.sh
    Fix issue with string literals producing missmatches

--- wanted/search-wanted.sh
+++ wanted/search-wanted.sh
@@ -5,14 +5,12 @@ declare WANTED_FILE=`dirname $0`/subs.wanted
 declare MISSED=""
 echo "###### Process started at: $(date) ######"
 
-while read -r line; do
-  IFS=':'
-  MAP_ARRAY=($(echo "$line"))
-  SOURCE=${MAP_ARRAY[0]}
-  SRT=${MAP_ARRAY[1]}
+while IFS=':' read -a line; do
+  SOURCE=${line[0]}
+  SRT=${line[1]}
   LANG=`echo $SRT | sed -e "s/\.srt//g" -e "s/.*\(..\)/\1/"`
   echo "subliminal download -l $LANG $SOURCE"
-  subliminal download -l $LANG $SOURCE
+  subliminal download -l $LANG "$SOURCE"
   if [[ ! -f $SRT ]]; then
     IFS=''
     MISSED="$SOURCE:$SRT\n$MISSED"
