36840860cb128385c2112653607736defa2c7da9
Cristian Miranda <crism60@gmail.com>
2020-05-11 18:53:45 -0300

    properties: replaced arguments with properties file; Sonarr v3 doesn't support args for custom scripts anymore.

--- sub-downloader.sh
+++ sub-downloader.sh
@@ -3,6 +3,9 @@ set -e
 echo `dirname $0`
 declare LOG_FILE=~/logs/subs-downloader.log
 declare WANTED_FILE=`dirname $0`/wanted/subs.wanted
+declare DOWNLOADER_PROPERTIES=`dirname $0`/downloader.properties
+
+source `dirname $0`/properties_parser.sh
 
 # Sonarr does not show the stdout as part of the log information displayed by the system,
 # So I decided to store the log information by my own.
@@ -12,33 +15,21 @@ function doLog {
 }
 
 function printUsage {
-  msg="Usage: sub-downloader.sh [options]\n\n
-    -l, --languages <languages-list>:\n
-    \t Specify a comma-separated list of languages to download.\n
-    \t example: sub-downloader.sh -l es,en\n\n
+  msg="Usage: sub-downloader.sh\n\n
+    This script relies on dowloader.properties, please fill \n\n
     -h, --help: print this help"
   doLog "$msg"
   exit 1
 }
 
-if [[ $# -eq 0 ]]; then
+if [[ "$1" == '-h' ]]; then
   printUsage
 fi
 
-while [ "$1" != "" ]; do
-  case $1 in
-    "-l" | "--languages")
-      shift
-      declare LANGUAGES=$(echo "-l $1" | sed "s/,/ -l /g")
-      ;;
-    *)
-      printUsage
-      ;;
-  esac
-  shift
-done
-
 doLog "###### Process started at: $(date) ######"
+
+LANGUAGES=`propLanguages $DOWNLOADER_PROPERTIES`
+CREDENTIALS=`propCredentials $DOWNLOADER_PROPERTIES`
 
 declare VIDEO_PATH=${sonarr_episodefile_path}
 if [[ -z $VIDEO_PATH ]]; then
@@ -53,10 +44,10 @@ fi
 
 doLog "Looking for subtitles for: ${VIDEO_PATH}"
 
-doLog "Executing subliminal"
-doLog "subliminal download ${LANGUAGES} ${VIDEO_PATH}"
-subliminal download ${LANGUAGES} "${VIDEO_PATH}" >> $LOG_FILE 2>&1
-  
+doLog "Executing subliminal, using $CREDENTIALS, and language $LANGUAGES"
+COMMAND="subliminal --debug $CREDENTIALS download $LANGUAGES \"$VIDEO_PATH\" >> $LOG_FILE 2>&1"
+eval $COMMAND   
+
 # Look for not found subtitles
 declare LANG_ARRAY=($(echo ${LANGUAGES} | sed "s/-l //g"))
 
