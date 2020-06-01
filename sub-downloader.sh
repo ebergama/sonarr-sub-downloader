3753d4ecb96464f09aa27aeccb4b53a35e5894f8
Ezequiel Bergamaschi <ezequiel@medallia.com>
2020-06-01 17:45:51 -0400

    Revert "Radarr support and properties file instead of arguments"

--- sub-downloader.sh
+++ sub-downloader.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 echo `dirname $0`
-declare LOG_FILE=~/logs/subs-downloader.log
+declare LOG_FILE=`dirname $0`/sub-downloader.log
 declare WANTED_FILE=`dirname $0`/wanted/subs.wanted
-declare DOWNLOADER_PROPERTIES=`dirname $0`/downloader.properties
-
-source `dirname $0`/properties_parser.sh
 
 # Sonarr does not show the stdout as part of the log information displayed by the system,
 # So I decided to store the log information by my own.
@@ -15,46 +12,54 @@ function doLog {
 }
 
 function printUsage {
-  msg="Usage: sub-downloader.sh\n\n
-    This script relies on dowloader.properties, please fill \n\n
+  msg="Usage: sub-downloader.sh [options]\n\n
+    -l, --languages <languages-list>:\n
+    \t Specify a comma-separated list of languages to download.\n
+    \t example: sub-downloader.sh -l es,en\n\n
     -h, --help: print this help"
   doLog "$msg"
   exit 1
 }
 
-if [[ "$1" == '-h' ]]; then
+if [[ $# -eq 0 ]]; then
   printUsage
 fi
 
+while [ "$1" != "" ]; do
+  case $1 in
+    "-l" | "--languages")
+      shift
+      declare LANGUAGES=$(echo "-l $1" | sed "s/,/ -l /g")
+      ;;
+    *)
+      printUsage
+      ;;
+  esac
+  shift
+done
+
 doLog "###### Process started at: $(date) ######"
 
-LANGUAGES=`propLanguages $DOWNLOADER_PROPERTIES`
-CREDENTIALS=`propCredentials $DOWNLOADER_PROPERTIES`
+declare EPISODE_PATH=${sonarr_episodefile_path}
 
-declare VIDEO_PATH=${sonarr_episodefile_path}
-if [[ -z $VIDEO_PATH ]]; then
-  doLog "Sonarr sonarr_episodefile_path not found. Maybe a Radarr movie?"
-  VIDEO_PATH=${radarr_moviefile_path}
-fi
-
-if [[ -z $VIDEO_PATH ]]; then
-  doLog "sonarr_episodefile_path not radarr_movie_path environment variable not found"
+if [[ -z $EPISODE_PATH ]]; then
+  doLog "sonarr_episodefile_path environment variable not found"
   exit 1
 fi
 
-doLog "Looking for subtitles for: ${VIDEO_PATH}"
+doLog "Looking for subtitles for: ${EPISODE_PATH}"
 
-doLog "Executing subliminal, using $CREDENTIALS, and language $LANGUAGES"
-COMMAND="subliminal --debug $CREDENTIALS download $LANGUAGES \"$VIDEO_PATH\" >> $LOG_FILE 2>&1"
-eval $COMMAND   
-
+doLog "Executing subliminal"
+doLog "subliminal download ${LANGUAGES} ${EPISODE_PATH}"
+subliminal download ${LANGUAGES} "${EPISODE_PATH}" >> $LOG_FILE 2>&1
+  
 # Look for not found subtitles
 declare LANG_ARRAY=($(echo ${LANGUAGES} | sed "s/-l //g"))
 
 for LANG in "${LANG_ARRAY[@]}"; do
-  SUB_FILE="${VIDEO_PATH%.*}.${LANG}.srt"
+  SUB_FILE=$(echo $EPISODE_PATH | sed "s/...$/${LANG}\.srt/g")
   if [[ ! -f $SUB_FILE ]]; then
     doLog "Subtitle ${SUB_FILE} not found, adding it to wanted"
-    echo $VIDEO_PATH:$SUB_FILE >> ${WANTED_FILE}
+    echo $EPISODE_PATH:$SUB_FILE >> ${WANTED_FILE}
   fi
 done
--- wanted/search-wanted.sh
+++ wanted/search-wanted.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-source `dirname $0`/../properties_parser.sh
-
-declare DOWNLOADER_PROPERTIES=`dirname $0`/../downloader.properties
 declare WANTED_FILE=`dirname $0`/subs.wanted
 
 declare MISSED=""
-echo "#########################################"
 echo "###### Process started at: $(date) ######"
-echo "#########################################"
-
-LANGUAGE=`propLanguages $DOWNLOADER_PROPERTIES`
-CREDENTIALS=`propCredentials $DOWNLOADER_PROPERTIES`
 
 while IFS=':' read -a line; do
   SOURCE=${line[0]}
   SRT=${line[1]}
-
-  COMMAND="subliminal $CREDENTIALS download $LANGUAGE \"$SOURCE\""
-
-  echo $COMMAND
-  eval $COMMAND
-  
+  LANG=`echo $SRT | sed -e "s/\.srt//g" -e "s/.*\(..\)/\1/"`
+  echo "subliminal download -l $LANG $SOURCE"
+  subliminal download -l $LANG "$SOURCE"
   if [[ ! -f $SRT ]]; then
     IFS=''
     MISSED="$SOURCE:$SRT\n$MISSED"
     echo "Subtitle still not available"
-  else 
+  else
     echo "Great! we have found $SRT"
   fi
 done < "$WANTED_FILE"
